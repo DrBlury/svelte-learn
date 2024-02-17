0734f5edd6ef2bcfde3df386c1409482fc638826
Julian Bensch <bensch.julian@gmail.com>
2024-02-17 01:50:19 +0100

    added xyflow and a custom node

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,61 @@
+<script>
+	import { writable } from 'svelte/store';
+	import { SvelteFlow, Controls, Background, BackgroundVariant, MiniMap } from '@xyflow/svelte';
+	import ColorPickerNode from '../nodes/ColorPickerNode.svelte';
+	import '@xyflow/svelte/dist/style.css';
+
+	const nodes = writable([
+		{
+			id: '1',
+			type: 'input',
+			data: { label: 'Input Node' },
+			position: { x: 0, y: 0 }
+		},
+		{
+			id: '2',
+			data: { label: 'Another Node' },
+			position: { x: 200, y: 0 }
+		},
+		{
+			id: '3',
+			type: 'output',
+			data: { label: 'Output Node' },
+			position: { x: 400, y: 0 }
+		},
+		{
+			id: '4',
+			type: 'default',
+			data: { label: 'Default Node' },
+			position: { x: 200, y: 200 }
+		},
+		{
+		id: '5',
+		type: 'color-picker',
+		data: { color: writable('#ff4000') },
+		position: { x: 200, y: 400 }
+		}
+	]);
+
+	const edges = writable([
+		{ id: 'e1-2', source: '1', target: '2', animated: true },
+		{ id: 'e2-3', source: '2', target: '3', animated: true },
+		{ id: 'e3-4', source: '3', target: '4', animated: true }
+	]);
+
+	const nodeTypes = {
+		'color-picker': ColorPickerNode
+	};
+</script>
+
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Home site" />
 </svelte:head>
 
-<section>
-	<h1>Welcome to the site</h1>
-	<div class="welcome">
-		<img src="/img/welcome.jpg" alt="Welcome to the site" />
-	</div>
-</section>
+<div style="height:100vh;">
+	<SvelteFlow {nodes} {edges} {nodeTypes} fitView attributionPosition="top-right">
+		<Controls />
+		<Background variant={BackgroundVariant.Dots} />
+		<MiniMap />
+	</SvelteFlow>
+</div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -28,12 +28,6 @@
 			<li aria-current={$page.url.pathname === '/inventories' ? 'page' : undefined}>
 				<a href="/inventories">Inventories</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/impress' ? 'page' : undefined}>
-				<a href="/impress">Impress</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
